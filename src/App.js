2580ce2939340eda7f04d4befc96728aca91b804
Fahadmohammad1 <205fahadmohammad@gmail.com>
2022-03-31 14:55:53 +0600

    navbar complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Header from "./components/Header/Header";
+import "flowbite";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Let's explore Mealdb using react router
-        </a>
-      </header>
+      <Header></Header>
     </div>
   );
 }
-
 export default App;
